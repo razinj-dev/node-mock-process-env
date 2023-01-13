625c43a6db556441a8882b1a9d75af820f717392
razinj <nizarjailane@gmail.com>
2023-01-13 21:24:48 +0100

    Add tests for default env values from setup file

--- app.test.js
+++ app.test.js
@@ -1,6 +1,45 @@
 const { appEnv, appVersion } = require('./app')
 
-describe('App Tests', () => {
+describe('App Tests - Default ENV Values', () => {
+  const originalEnv = process.env
+
+  afterEach(() => {
+    // Reset process.env after each test case
+    process.env = originalEnv
+  })
+
+  describe('APP_VERSION Tests', () => {
+    it('should return default value when APP_VERSION', () => {
+      const result = appVersion()
+
+      expect(result).toBe(`App's version is v0.1`)
+    })
+
+    it('should throw an error if APP_VERSION is not defined', () => {
+      process.env = { APP_VERSION: undefined }
+      // OR process.env = {}
+
+      expect(appVersion).toThrow('APP_VERSION is not defined')
+    })
+  })
+
+  describe('NODE_ENV Tests', () => {
+    it('should return default value when NODE_ENV', () => {
+      const result = appEnv()
+
+      expect(result).toBe('App is running on local environment')
+    })
+
+    it('should throw an error if NODE_ENV is not defined', () => {
+      process.env = { NODE_ENV: undefined }
+      // OR process.env = {}
+
+      expect(appEnv).toThrow('NODE_ENV is not defined')
+    })
+  })
+})
+
+describe('App Tests - Manual ENV Values', () => {
   const originalEnv = process.env
 
   beforeEach(() => {
